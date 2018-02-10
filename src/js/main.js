adc0795f7c5326ea99a5c5ea2718c1050ecc5f93
prod1992 <levon1.grigoryan@gmail.com>
2018-02-11 02:06:37 +0400

    updates

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,12 @@
 
     $('.navbar-toggler').on('click', function () {
         $('body').toggleClass('menu-open');
+
+        if($('body').hasClass('menu-open')){
+            $('body').addClass("scroll-off");
+        } else{
+            $('body').removeClass("scroll-off");
+        }
+
     });
 })(jQuery);
