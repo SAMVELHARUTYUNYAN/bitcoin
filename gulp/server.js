5e29f599b3c1a24fa801108f4995436a46f61d98
prod1992 <levon1.grigoryan@gmail.com>
2018-02-12 08:30:49 +0400

    updates

--- gulp/server.js
+++ gulp/server.js
@@ -11,37 +11,75 @@ var browserSyncSpa = require('browser-sync-spa');
 
 var util = require('util');
 
-function browserSyncInit (baseDir, browser) {
-  browser = browser === undefined ? 'default' : browser;
+var express = require('express');
 
-  var routes = null;
-  if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
-    routes = {
-      '/bower_components': 'bower_components'
+
+var proxyMiddleware = require('http-proxy-middleware');
+
+function browserSyncInit(baseDir, browser) {
+    browser = browser === undefined ? 'default' : browser;
+
+    var routes = null;
+    if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
+        routes = {
+            '/bower_components': 'bower_components'
+        };
+    }
+
+    var server = {
+        baseDir: baseDir,
+        routes: routes
     };
-  }
+    var proxyOptions = {
+        target: pkg.api,
+        changeOrigin: true
+        // logLevel: 'debug',
+        // onProxyReq: function (proxyReq, req, res) {
+        //    console.log(proxyReq);
+        //  }
+    };
 
-  var server = {
-    baseDir: baseDir,
-    routes: routes
-  };
+    server.middleware = proxyMiddleware('/', proxyOptions);
 
+    browserSync.instance = browserSync.init({
+        startPath: '/',
+        server: server,
+        open: process.env.PORT ? false : true,
+        notify: process.env.PORT ? false : true,
+        ghostMode: false,
+        port: process.env.PORT || 3000,
+        browser: browser
+    });
+}
 
-  browserSync.instance = browserSync.init({
-    startPath: '/',
-    server: server,
-    open: process.env.PORT ? false : true,
-    notify: process.env.PORT ? false : true,
-    ghostMode: false,
-    port: process.env.PORT || 3000,
-    browser: browser
-  });
+function prodServerInit(baseDir) {
+
+    var proxyOptions = {
+        target: pkg.api,
+        changeOrigin: true,
+        // logLevel: 'debug',
+        // onProxyReq: function (proxyReq, req, res) {
+        //
+        // }
+    };
+
+    var app = express();
+
+    app.use(express.static(baseDir));
+    app.use('/api', proxyMiddleware(proxyOptions));
+
+    app.get('*', function(req, res) {
+        res.sendFile('index.html', {root: baseDir });
+    });
+
+    app.listen(process.env.PORT || 3000);
+
 }
 
 gulp.task('serve', ['watch', 'images', 'fonts'], function () {
-  browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.favicons, conf.paths.src]);
+    browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.favicons, conf.paths.src]);
 });
 
 gulp.task('serve:dist', ['build'], function () {
-  browserSyncInit(conf.paths.dist);
+    prodServerInit(conf.paths.dist);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,28 @@
-(function($) {
+(function ($) {
+    $.ajax({
+        url: "/api",
+        success: function (result) {
+            var bitData = $(result).find('#TabStats').find('td:contains("ROI")').next('td').text();
+            $('.page-hero--inner').append($('<div class="fv-roi"> ROI - ' + bitData + '</div>'));
+        }
+    });
+    setInterval(ajaxCall, 7000);
+    function ajaxCall() {
+        $.ajax({
+            url: "/api",
+            success: function (result) {
+                var bitData = $(result).find('#TabStats').find('td:contains("ROI")').next('td').text();
 
+                $('.page-hero--inner .fv-roi').text('ROI - ' + bitData);
+            }
+        });
+    }
     $('.navbar-toggler').on('click', function () {
         $('body').toggleClass('menu-open');
 
-        if($('body').hasClass('menu-open')){
+        if ($('body').hasClass('menu-open')) {
             $('body').addClass("scroll-off");
-        } else{
+        } else {
             $('body').removeClass("scroll-off");
         }
 
