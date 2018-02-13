7015eccadc14afc79ae1becf743c5b2ba101ae60
Levon Grigoryan <levon@bandwango.com>
2018-02-13 10:50:30 +0400

    fixes

--- gulp/images.js
+++ gulp/images.js
@@ -5,19 +5,10 @@
     var path = require('path');
     var gulp = require('gulp');
     var conf = require('./conf');
-    var imagemin = require('gulp-imagemin');
 
     // images task
     gulp.task('images', function () {
-        console.debug(this.seq.slice(-1)[0]);
         return gulp.src(path.join(conf.paths.src, '/_assets/images/**/*'))
-            .pipe(imagemin({
-                interlaced: true,
-                progressive: true,
-                optimizationLevel: 5,
-                verbose: true,
-                svgoPlugins: [{removeViewBox: true}]
-            }))
             .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/images')))
             .pipe(gulp.dest(path.join(conf.paths.dist, '/images')));
     });
