fac61c8c4ec7f14587b21274dc8c37ece1b05c57
Levon Grigoryan <levon@bandwango.com>
2018-02-14 07:23:10 +0400

    working index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
     var express = require('express');
     var proxyMiddleware = require('http-proxy-middleware');
     var conf = require('./gulp/conf');
+    var compression = require('compression');
     var proxyOptions = {
         target: 'https://masternodes.online/currencies/BLT',
         changeOrigin: true
@@ -16,12 +17,15 @@
 
     var app = express();
 
+    app.use(compression());
+
     app.use(express.static(conf.paths.dist));
 
     app.use('/api', proxyMiddleware(proxyOptions));
 
     app.get('/', function(req, res, next) {
-        res.sendFile('index.html', {root: conf.paths.dist });
+        res.send('index.html', {root: conf.paths.dist });
+        next();
     });
 
     app.listen(8080);
